043f4a20365999552af6391a6ba1bae7c73708bb
Lariel N. Santos <lariel.negreiros@gmail.com>
2018-06-15 23:44:13 -0300

    cria usuário sem nick, somente email e pswd

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    // User is signed in.
+    // já estava logado
 
     document.getElementById("conversa").style.display = "block";
     document.getElementById("login_div").style.display = "none";
@@ -16,7 +16,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     }
 
   } else {
-    // No user is signed in.
+    // não logado
     document.getElementById("login_div").style.display = "block";
     document.getElementById("conversa").style.display = "none";
     document.getElementById("sigup_div").style.display = "none";
@@ -30,18 +30,15 @@ function login(){
   var userPass = document.getElementById("password_field").value;
 
   firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-    // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
 
     window.alert("Error : " + errorMessage);
-
-    // ...
   });
 
 }
 
-function gosignup(){
+function gosignup(){ //somente para ligação da tela de login com a de registro de usuário
   document.getElementById("signup_div").style.display = "block";
   document.getElementById("login_div").style.display = "none";
 }
@@ -50,8 +47,36 @@ function signup(){
   var novoUserNick = document.getElementById("new_nick_field").value;
   var novoUserEmail = document.getElementById("new_email_field").value;
   var novoUserPass = document.getElementById("new_password_field").value;
-  
-  window.alert("bem vindo, "+novoUserNick+" e-mail: "+novoUserEmail);
+
+  firebase.auth().createUserWithEmailAndPassword(novoUserEmail, novoUserPass, )
+    .catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  if (errorCode == 'auth/weak-password') {
+    alert('The password is too weak.');
+  } else {
+    alert(errorMessage);
+  }
+  console.log(error);
+});
+
+/*
+admin.auth().createUser({
+  email: novoUserEmail,
+  emailVerified: false,
+  password: novoUserPass,
+  displayName: novoUserNick,
+  disabled: false
+})
+  .then(function(userRecord) {
+    window.alert("Sucesso, usuário "+novoUserNick+" criado!"+userRecord.uid+" - "+userRecord.displayName);
+  })
+  .catch(function(error) {
+    window.alert("Erro ao criar usuário: ", error);
+  });
+
+  */
 
   document.getElementById("login_div").style.display = "block";
   document.getElementById("signup_div").style.display = "none";
